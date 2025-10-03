dd0321c3395928144dfd532062128c8c8b55f8e3
devesh1011 <devesh.22scse1100004@galgotiasuniversity.edu.in>
2025-10-03 13:49:51 +0530

    feat: add EventsRoute component to render EventsPage

---
+++ frontend/src/app/events/page.tsx
@@ -0,0 +1,9 @@
+import { EventsPage } from "@/components/events/events-page";
+
+export default function EventsRoute() {
+  return (
+    <div className="container mx-auto p-6 max-w-7xl">
+      <EventsPage />
+    </div>
+  );
+}
